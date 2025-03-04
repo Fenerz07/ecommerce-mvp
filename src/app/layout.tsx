d4d0c08a054b0f931bbb0a80efbc3ef21addc74b
Fenerz07 <hugom2005@free.fr>
2025-03-04 22:58:27 +0100

    homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import  '../styles/globals.css';
 import Header from '../components/header';
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,82 @@
+import styles from '../styles/home.module.css';
+import Image from 'next/image';
+
+const products = [
+  { src: '/toy.jpg', alt: 'Product1', title: 'BloomToy', price: 'PRIX' },
+  { src: '/bag.jpg', alt: 'Product2', title: 'LetterBag', price: 'PRIX' },
+  { src: '/tshirt.jpg', alt: 'Product3', title: 'BloomShirt', price: 'PRIX' },
+];
+
+const nouveautes = [
+  {
+    id: 1,
+    title: "Le Certificat d'Authenticité",
+    description:
+      "Pour vous remercier d'acheter un produit de notre gamme luxe 'Fait Main', la marque vous offre maintenant un certificat qui prouve que votre article est authentique, unique et original.",
+    video: "/file3.mp4",
+  },
+  {
+    id: 2,
+    title: "L'Encyclopédie",
+    description:
+      "Fonctionnant comme un 'Pokedex', ce bestiaire nommé 'Encyclopédie' référence tous les personnages et créatures de l'histoire et sera régulièrement mis à jour en fonction de l'avancée de l'histoire.",
+    video: "/file3.mp4",
+  },
+  {
+    id: 3,
+    title: "Les Cartes de Récit",
+    description:
+      "Pour tout achat d'un tee-shirt ou d'un article de la rubrique 'Boutique', vous recevrez une petite carte de Bienvenue afin de vous plonger dans notre histoire...",
+    video: "/file3.mp4",
+  },
+];
+
 export default function Home() {
   return (
-    <></>
+    <>
+      <div className={styles.video}>
+        <video autoPlay loop muted>
+          <source src="/file.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+      <div className={styles.about}>
+        <video autoPlay loop muted>
+          <source src="/file2.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <h1>Lambda Story</h1>
+        <p>
+          Lambda Story - &quot;Pas Seulement une marque de vêtement.. Mais un monde de Fantaisie et de Mystère, venez, installez-vous et profitez du voyage car BloomBoy arrive&quot;
+        </p>
+      </div>
+      <div className={styles.products}>
+        {products.map((product, index) => (
+          <div key={index} className={styles.product}>
+            <Image src={product.src} alt={product.alt} width={300} height={300} />
+            <h2>{product.title}</h2>
+            <p>{product.price}</p>
+          </div>
+        ))}
+      </div>
+      <section className={styles.nouveautes}>
+        <h2 className={styles.title}>Les Nouveautés</h2>
+        <div className={styles.container}>
+          {nouveautes.map((item) => (
+            <div key={item.id} className={styles.card}>
+              <div className={styles.number}>{item.id}</div>
+              <h3 className={styles.cardTitle}>{item.title}</h3>
+              <p className={styles.description}>{item.description}</p>
+              {item.video && (
+                <video className={styles.video} autoPlay muted loop width="50" height="50">
+                  <source src={item.video} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              )}
+            </div>
+          ))}
+        </div>
+      </section>
+    </>
   );
 }
