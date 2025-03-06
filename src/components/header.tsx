201f60e892d61c053e3441e74ae0c856b9bf7348
Fenerz07 <hugom2005@free.fr>
2025-03-06 23:22:15 +0100

    burger nav

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,18 +1,41 @@
+'use client';
+
+import React, { useState } from 'react';
 import Image from 'next/image';
 import styles from '../styles/header.module.css';
 import Links from 'next/link';
 
 export default function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <header className={styles.header}>
-            <div className={styles.burgerMenu}>
-                    <Image src="/svg/menu.svg" alt="Burger Menu" width={30} height={30} />
+            <div className={styles.burgerMenu} onClick={toggleMenu}>
+                <Image src="/svg/menu.svg" alt="Burger Menu" width={30} height={30} />
             </div>
             <div className={styles.logo}>
                 <Links href="/" passHref>
                     <Image src="/logo.avif" alt="Logo" width={150} height={100} />
                 </Links>
             </div>
+            <nav className={`${styles.nav} ${isMenuOpen ? styles.open : ''}`}>
+                <Links href="/" passHref className={styles.navLink}>
+                    Home
+                </Links>
+                <Links href="/about" passHref className={styles.navLink}>
+                    About
+                </Links>
+                <Links href="/products" passHref className={styles.navLink}>
+                    Products
+                </Links>
+                <Links href="/contact" passHref className={styles.navLink}>
+                    Contact
+                </Links>
+            </nav>
             <div className={styles.cartAndConnect}>
                 <div className={styles.cart}>
                     <Links href="/cart" passHref>
@@ -27,5 +50,5 @@ export default function Header() {
                 </div>
             </div>
         </header>
-    ); 
+    );
 }
