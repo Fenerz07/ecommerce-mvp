0f7f702ca7b7b639716bf8ce3fcf8e79a16feca8
Fenerz07 <hugom2005@free.fr>
2025-03-06 23:27:50 +0100

    about

---
+++ src/app/about/page.tsx
@@ -0,0 +1,24 @@
+import styles from '../../styles/links/about.module.css';
+
+export default function About() {
+    return (
+        <div className={styles.aboutContainer}>
+            <h1>About LambdaStory</h1>
+            <p>
+                LambdaStory vous pr&eacute;sente une histoire fantastique dont le personnage principal est BloomBoy.
+            </p>
+            <p>
+                A l&apos;avenir vous allez d&eacute;couvrir un monde riche en couleurs et en valeurs. Avec plein de personnages et de r&eacute;f&eacute;rences.
+            </p>
+            <p>
+                LambdaStory, c&apos;est l&apos;histoire d&apos;un petit gar&ccedil;on nomm&eacute; BloomBoy qui traverse un monde magique sans aucun souvenir de son pass&eacute; ici.
+            </p>
+            <p>
+                Accompagn&eacute; d&apos;un gros lapin blanc magique, Bloom Boy semble &ecirc;tre choisi pour prot&eacute;ger ce monde et le d&eacute;couvrir.
+            </p>
+            <p>
+                Entre recherche d&apos;identit&eacute; et exploration, venez profiter d&apos;un voyage hors du commun et d&eacute;couvrir de nouveaux horizons.
+            </p>
+        </div>
+    );
+}
