c70aa00250df6a8a89f8c2bc1118cfd7e3eefab3
Fenerz07 <hugom2005@free.fr>
2025-03-07 23:35:38 +0100

    header responsivness

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -21,38 +21,59 @@ export default function Header() {
     return (
         <header className={styles.header}>
             <div className={styles.burgerMenu} onClick={toggleMenu}>
-                <Image src="/svg/menu.svg" alt="Burger Menu" width={30} height={30} />
+                <Image src="/svg/menu.svg" alt="Burger Menu" width={50} height={50} />
             </div>
+            {isMenuOpen && (
+                <div className={styles.mobileNav}>
+                    <Links href="/" passHref className={styles.navLink} onClick={closeMenu}>
+                        Accueil
+                    </Links>
+                    <Links href="/about" passHref className={styles.navLink} onClick={closeMenu}>
+                        Nous
+                    </Links>
+                    <Links href="/products" passHref className={styles.navLink} onClick={closeMenu}>
+                        Produits
+                    </Links>
+                    <Links href="/contact" passHref className={styles.navLink} onClick={closeMenu}>
+                        Contacter
+                    </Links>
+                    <Links href="/account" passHref className={styles.navLinkAccount} onClick={closeMenu}>
+                        <Image src="/svg/login.svg" alt="Connect" width={30} height={30} />
+                        Se connecter
+                    </Links>
+                </div>
+            )}
             <div className={styles.logo}>
                 <Links href="/" passHref>
                     <Image src="/logo.avif" alt="Logo" width={150} height={100} />
                 </Links>
             </div>
-            <nav className={`${styles.nav} ${isMenuOpen ? styles.open : ''}`}>
-                <Links href="/" passHref className={styles.navLink} onClick={closeMenu}>
+            <nav className={styles.nav}>
+                <Links href="/" passHref className={styles.navLink}>
                     Accueil
                 </Links>
-                <Links href="/about" passHref className={styles.navLink} onClick={closeMenu}>
-                    Nous
+                <Links href="/about" passHref className={styles.navLink}>
+                    Histoire
                 </Links>
-                <Links href="/products" passHref className={styles.navLink} onClick={closeMenu}>
+                <Links href="/products" passHref className={styles.navLink}>
                     Produits
                 </Links>
-                <Links href="/contact" passHref className={styles.navLink} onClick={closeMenu}>
-                    Contacter
+                <Links href="/contact" passHref className={styles.navLink}>
+                    Contact
                 </Links>
             </nav>
             <div className={styles.cartAndConnect}>
                 <div className={styles.cart}>
                     <Links href="/cart" passHref>
-                        <Image src="/svg/cart.svg" alt="Cart" width={100} height={30} />
+                        <Image src="/svg/cart.svg" alt="Cart" width={30} height={30} />
                     </Links>
-                    <p>panier ({cartItemCount})</p>
+                    <p>Panier ({cartItemCount})</p>
                 </div>
                 <div className={styles.connect}>
                     <Links href="/account" passHref>
                         <Image src="/svg/login.svg" alt="Connect" width={30} height={30} />
                     </Links>
+                    <p>Se connecter</p>
                 </div>
             </div>
         </header>
